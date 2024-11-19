db5ae70eeb7d93b7ffff349d1b5fa9073c0f2165
anabsanchez <anabsanchez.dev@gmail.com>
2024-11-19 20:53:44 +0000

    feat: implemented basic logic for Farmer thread

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import net.salesianos.elements.Farmer;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Farmer farmer1 = new Farmer("Paco", 5);
+        Farmer farmer2 = new Farmer("Lucía", 3);
+        Farmer farmer3 = new Farmer("Trululú", 7);
+
+        Thread farmerThread1 = new Thread(farmer1);
+        farmerThread1.start();
+
+        Thread farmerThread2 = new Thread(farmer2);
+        farmerThread2.start();
+
+        Thread farmerThread3 = new Thread(farmer3);
+        farmerThread3.start();
+
+        try {
+
+            farmerThread1.join();
+            farmerThread2.join();
+            farmerThread3.join();
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
     }
 }
--- src/net/salesianos/elements/Farmer.java
+++ src/net/salesianos/elements/Farmer.java
@@ -17,6 +17,26 @@ public class Farmer extends Person implements Runnable {
     @Override
     public void run() {
 
+        for (int i = 0; i < harvestCount; i++) {
+            
+            try {
 
+                String crop = CROPS[(int) (Math.random() * CROPS.length)];
+
+                System.out.println("\n(Harvest " + (i + 1) + "/" + harvestCount + ") " 
+                                    + this.getName() + " is planting " + crop + " in the field...");
+
+                long cultivationTime = (long) (Math.random() * 5000 )+ 1000;
+                Thread.sleep(cultivationTime);
+
+
+                System.out.println("\n" + this.getName() + " has successfully delivered " + crop + " to the restaurant.");
+
+            } catch (InterruptedException e) {
+                
+                System.out.println("\n[ERROR] " + getName() + " was interrupted while cultivating.");
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
