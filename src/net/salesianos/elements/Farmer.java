bfd138d7c0123800fb7550f0c18f6837b397d4dc
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2024-11-25 18:31:16 +0000

    feat(Farmer): decreased the cultivation time

--- src/net/salesianos/elements/Farmer.java
+++ src/net/salesianos/elements/Farmer.java
@@ -3,16 +3,16 @@ package net.salesianos.elements;
 public class Farmer extends Person implements Runnable {
 
     private static int cropCounter = 0;
-    
-    private final String[] CROPS = {"lettuce", "cabbage", "onion", "spinach", "potato",
-                                "celery", "asparagus", "radish", "broccoli", "artichoke",
-                                "tomato", "cucumber", "eggplant", "carrot", "green bean"};
-    
+
+    private final String[] CROPS = { "lettuce", "cabbage", "onion", "spinach", "potato",
+            "celery", "asparagus", "radish", "broccoli", "artichoke",
+            "tomato", "cucumber", "eggplant", "carrot", "green bean" };
+
     private int harvestCount;
     private Restaurant restaurant;
 
     public Farmer(String name, int harvestCount, Restaurant restaurant) {
-        
+
         super(name);
         this.harvestCount = harvestCount;
         this.restaurant = restaurant;
@@ -22,17 +22,18 @@ public class Farmer extends Person implements Runnable {
     public void run() {
 
         for (int i = 0; i < harvestCount; i++) {
-            
+
             try {
 
                 String crop = CROPS[(int) (Math.random() * CROPS.length)];
                 String cropId = "C" + (++cropCounter);
 
                 System.out.println("\n[Farmer: " + getName() + "] 🌱 Planting \"" + crop + "\" (ID: " + cropId + ")");
-                long cultivationTime = (long) (Math.random() * 5000) + 1000;
+                long cultivationTime = (long) (Math.random() * 2000) + 1000;
                 Thread.sleep(cultivationTime);
 
-                System.out.println("\n[Farmer: " + getName() + "] ✅ Delivering \"" + crop + "\" (ID: " + cropId + ") to the restaurant...");
+                System.out.println("\n[Farmer: " + getName() + "] ✅ Delivering \"" + crop + "\" (ID: " + cropId
+                        + ") to the restaurant...");
                 restaurant.addCrop(cropId + ": " + crop);
 
             } catch (InterruptedException e) {
